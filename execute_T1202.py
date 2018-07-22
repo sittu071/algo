e3db9b2c70a6c7595e6959c6b5fa8f77cdb1f304
mausam sinha <mausam-kumar.sinha@hpe.com>
2018-07-22 11:20:47 +0530

    removed startup function from the execute_T1202.py

--- execute_T1202.py
+++ execute_T1202.py
@@ -1,8 +1,12 @@
-####################################################################################################
-#   Name   :    execute_T1202.py                 Creation :  25/04/18                              #
-####################################################################################################
-#    Author :    Mausam Kumar Sinha                                                                #
-####################################################################################################
+######################################################################################################
+#   Name   :    execute_T1202.py                 Creation :  25/04/18                                #
+######################################################################################################
+#    Author :    Mausam Kumar Sinha                                                                  #
+######################################################################################################
+#-------------------------------Authors Notes--------------------------------------------------------#
+#this program has written in the sense of the user already set execute the setup env variable inside
+#rootfolder and current directory, for any clarification feel free to ask at mausam-kumar.sinha@hpe.com
+
 import os
 import re                             #Regular expression
 import datetime
@@ -47,37 +51,6 @@ def cleanup(): #delete the exsiting file if any
         pass
 
 
-def startup(): #start the executing code
-    if  os.path.isdir("/TETARUL"):
-        os.chdir("/TETARUL")
-    elif os.path.isdir("/TETSVN"):
-        os.chdir("/TETSVN")
-    else:
-        print("Oops /TETSVN or /TETARUL directory not exist")
-
-    currentdirectory = os.getcwd()
-    if os.system(". ./setup.env"):
-        print("setup environment successfully done")
-    else:
-        print("Not able to set environment variable try to do manually inside ", currentdirectory)
-    os.chdir(currentworkingdirectory)
-    currentdirectory = os.getcwd()
-    print("Current Directory is ", currentdirectory)
-    if os.system(". ./setup.env"):
-        print("setup environment successfully done")
-    else:
-        print("Not able to set environment variable try do manually inside ", currentdirectory)
-    tempvar = "export TET_EXECUTE=" + currentdirectory
-    # exporting the current directory to make a tet executable
-    if os.system(tempvar):
-        print("TET_EXECUTE successfully exported")
-    else:
-        print("Not able to set TET_EXECUTE try to do manually inside  ", currentdirectory)
-    print("Trying to find tet_scen file inside ", currentdirectory)
-    path = "/usr/coreutils/bin:.:/bin:/usr/bin:/usr/ucb:/TETARUL/bin://bin:/usr/tandem/nssjava/jdk170_h70/bin"
-    path = "export PATH=" + path
-    os.system(path)
-
 def mainmethod():
 
     with open('tet_scen', 'r') as file:
@@ -116,7 +89,14 @@ def result():
     os.system("/TETARUL/src/tet3/grw/grw -c beTIpfix all_journal > T1202COREUTILS.html")
 
 
-#cleanup()
-startup()
-mainmethod()
-result()
+def T1202Main_Fun():
+    cleanup()
+    mainmethod()
+    result()
+
+
+if __name__ == '__main__':
+    T1202Main_Fun()
+
+
+
